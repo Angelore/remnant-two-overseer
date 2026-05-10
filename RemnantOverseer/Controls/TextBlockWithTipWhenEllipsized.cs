2bae31f0479f69c3c212496fdaca48e271810b37
Angelore <angelore.main@gmail.com>
2026-05-11 00:27:07 +0300

    Replace attached property extension with new control; add dividers to some tips

---
+++ RemnantOverseer/Controls/TextBlockWithTipWhenEllipsized.cs
@@ -0,0 +1,66 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using System;
+using System.Linq;
+
+namespace RemnantOverseer.Controls;
+public class TextBlockWithTipWhenEllipsized : TextBlock
+{
+    static TextBlockWithTipWhenEllipsized()
+    {
+        TextProperty.Changed.AddClassHandler<TextBlockWithTipWhenEllipsized>(OnTextChanged);
+        TextTrimmingProperty.Changed.AddClassHandler<TextBlockWithTipWhenEllipsized>(OnTextTrimmingChanged);
+    }
+
+    public TextBlockWithTipWhenEllipsized() { }
+
+    private static void OnTextChanged(TextBlockWithTipWhenEllipsized tb, AvaloniaPropertyChangedEventArgs e)
+    {
+        tb.UpdateTip();
+    }
+
+    private static void OnTextTrimmingChanged(TextBlockWithTipWhenEllipsized tb, AvaloniaPropertyChangedEventArgs e)
+    {
+        tb.UpdateTip();
+    }
+
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToVisualTree(e);
+        LayoutUpdated += OnLayoutUpdated;
+        UpdateTip();
+    }
+
+    protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnDetachedFromVisualTree(e);
+        LayoutUpdated -= OnLayoutUpdated;
+    }
+
+    private void OnLayoutUpdated(object? sender, EventArgs e) => UpdateTip();
+
+    private void UpdateTip()
+    {
+        var tip = IsEllipsized(this) ? Text : null;
+        if (Equals(ToolTip.GetTip(this), tip))
+        {
+            return;
+        }
+        ToolTip.SetTip(this, tip);
+    }
+
+    private static bool IsEllipsized(TextBlock textBlock)
+    {
+        if (textBlock.TextTrimming == TextTrimming.None)
+        {
+            return false;
+        }
+
+        var layout = textBlock.TextLayout;
+        if (layout == null)
+            return false;
+
+        return layout.TextLines.Any(line => line.HasCollapsed);
+    }
+}
